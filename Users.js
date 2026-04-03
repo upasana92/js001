4baa791d5cbc65521e91d41d6bc5dd78dc266163
upasana92 <143943653+upasana92@users.noreply.github.com>
2026-04-03 12:42:13 +0530

    Node Js Crud Operations

---
+++ Users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id: 1,
+        name: "Myname",
+        email: "me@gmail.com"
+    },
+    {
+        id: 2,
+        name: "yourname",
+        email:"uou@gmail.com"
+    },
+    {
+        id: 3,
+        name: "hername",
+        email:"her@gmail.com"
+    },
+    {
+        id: 4,
+        name: "hisname",
+        email: "his@gmail.com"
+    },
+];
+
+module.exports = users;
